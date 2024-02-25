b2f06c4b911c7847be17d4f66433ee4ba7e2db14
AkifPinarci <mehmetakifpinarci@gmail.com>
2024-02-26 00:02:34 +0100

    New props

--- main.py
+++ main.py
@@ -1,3 +1,143 @@
-print("Mehmet Akif Pinarci")
-for i in range(10):
-    pass
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+# print(torch.__version__)
+
+# Creating Tensors
+# Scalar
+
+# scalar = torch.tensor(7)
+# print(scalar)
+# print(scalar.ndim)
+# # Get tensor back as python int
+# print(scalar.item())
+
+# Vector
+# vector = torch.tensor([7, 7])
+# print(vector)
+# print(vector.ndim)
+# print(vector.shape)
+
+# Matrix
+# MATRIX = torch.tensor([[7, 8],
+#                         [9, 10]])
+# print(MATRIX)
+# print(MATRIX.ndim)
+# print(MATRIX[1])
+# print(MATRIX.shape)
+
+# TENSOR
+# TENSOR = torch.tensor([[[1, 2, 3],
+#                         [3, 6, 9], 
+#                         [2, 4, 5]]])
+# print(TENSOR)
+# print(TENSOR.ndim)
+# print(TENSOR.shape)
+# print(TENSOR[0])
+# print(TENSOR[0, 1])
+# print(TENSOR[0, 1, 1])  
+
+### Random Tensors
+# Start with random numbers -> look at data -> update random numbers -> look at data -> update random numbers
+
+#Create a random tensors of size (3, 4)
+# random_tensor = torch.rand(3, 4)
+# print(random_tensor)
+# print(random_tensor.ndim)
+
+# Create a random tensor with similar shape to an image tensor
+# random_image_size_tensor = torch.rand(size = (3, 224, 224)) # color channels, height, width
+# print(random_image_size_tensor.shape)
+# print(random_image_size_tensor.ndim)
+# print(random_image_size_tensor)
+
+# Create tensor with all zeros
+# zeros = torch.zeros(3, 4)
+# print(zeros[0] * random_tensor)
+
+# Create a tensor with all ones
+# ones = torch.ones(3, 4)
+# print(ones)
+# print(ones.dtype)
+
+# Creating a range of tensros and tensors-like
+# one_to_ten = torch.arange(start = 1, end = 11, step = 1)
+
+# Creating tensors_like
+# ten_zeros = torch.zeros_like(one_to_ten)
+# one_zeros = torch.ones_like(one_to_ten)
+
+
+# Float 32 Tensor
+# 1. Tensors not right datatypes
+# 2. Tensors not right shape
+# 3. Tensors not on the right device 
+# float_32_tensor = torch.tensor([3.0, 6.0, 9.0], 
+#                                dtype= None, # What data type is the tensor
+#                                device = None, # What device is your tensor on
+#                                requires_grad=False) # Whether or not to track gradients with this tensors operations
+# print(float_32_tensor)
+
+# flaot_16_tensor = float_32_tensor.type(torch.float16)
+# print(flaot_16_tensor * float_32_tensor)
+
+# int_32_tensor = torch.tensor([3, 6, 9], dtype=torch.long)
+# print(int_32_tensor * float_32_tensor)
+
+# Getting information from tensors
+# 1. Tensors not right datatypes - to get datatype from a tensor, can use "tensor.dtype"
+# 2. Tensors not right shape - to get shape from a tensor, can use "tensor.shape"
+# 3. Tensors not on the right device - to get device from a tensor, can use "tensor.device"
+
+# Create tensors
+# tensor = torch.tensor([1, 2, 3])
+# print(f"Datatype of tensor: {some_tensor.dtype}")
+# print(f"Shape of tensor: {some_tensor.shape}")
+# print(f"Device tensor is on: {some_tensor.device}")
+
+# Tensor operations
+# Addition - 
+# Substraction - 
+# Multiplication(element-wise)
+# Division
+# Matrix multiplicaiton
+# print(tensor + 10)
+# print(tensor * 10)
+# print(tensor - 10)
+# print(tensor / 10)
+
+# tensor = torch.mul(tensor, 10)
+# print(tensor)
+
+# Matrix multiplication
+# Two main ways of performing multiplication in neural newworks and deep learning:
+# 1. Element-wise multiplication
+# 2. Matrix multiplication (Dot Product)
+# tensor = torch.tensor([1, 2, 3])
+
+# print(torch.matmul(tensor, tensor))
+# print(tensor @ tensor)
+
+# # .T takes the transpose of the tensor
+# print(torch.matmul(torch.rand(3, 2).T, torch.rand(3, 2)))
+
+# Finding the min, max, mean, sum, etc (tensor aggregation)
+# x = torch.arange(0, 100, 10)
+# print(x.dtype)
+# print(x)
+# print(torch.min(x))
+# print(x.min())
+# print(torch.max(x))
+# print(x.max())
+# print(torch.mean(x.type(torch.float32)))
+# print(x.type(torch.float32).mean())
+# print(torch.sum(x))
+# print(x.sum())
+
+# Finding the positional min and max
+x = torch.arange(0, 100, 10)
+# Find the position in tensor that hhas the minimum value with the argmin() -> returns index position of target tensor where the min value occurs
+print(x.argmin())
+print(x.argmax())
